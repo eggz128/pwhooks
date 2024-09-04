0cbb9ab8a613ad29e2b198949c10e23143a74e28
Stephen Powell <steve.powell@edgewords.co.uk>
2024-09-04 14:27:46 +0100

    Tweaked comments for Edgewords playwright course slides

--- tests/globalbeforeafter.ts
+++ tests/globalbeforeafter.ts
@@ -1,7 +1,8 @@
 import { test as base } from '@playwright/test';
 
+//Adding beforeEach+afterEach and beforeAll+afterAll functionality to any test files that import test() from this file
 export const test = base.extend<{ forEachTest: void }, { forEachWorker: void }>({
-  forEachTest: [async ({ page }, use) => {
+  forEachTest: [async ({ page }, use) => { //analagous to beforeEach+afterEach 
     // This code runs 'before each' test.
     console.log('Global beforeEach')
     await page.goto('http://www.google.com');
@@ -16,7 +17,7 @@ export const test = base.extend<{ forEachTest: void }, { forEachWorker: void }>(
   }, { auto: true }],  // automatically starts for every test.
 
 
-  forEachWorker: [async ({ }, use) => {
+  forEachWorker: [async ({ }, use) => { //analagous to beforeAll+afterAll
     // This code runs 'before all' the tests in the worker process.
     console.log('Global beforeAll')
     console.log(`Starting test worker ${test.info().workerIndex}`);
@@ -25,7 +26,9 @@ export const test = base.extend<{ forEachTest: void }, { forEachWorker: void }>(
     await use(); //Do the test.
 
     // This code runs 'after all' the tests in the worker process.
-    //console.log(`Stopping test worker ${test.info().workerIndex}`); //From PLaywright documentation example code but fails test as when this runs the test has already exited, so cant call test.info
+    //From PLaywright documentation example code but fails test as when this runs the test has already exited,
+    //so cant call test.info
+    //console.log(`Stopping test worker ${test.info().workerIndex}`); 
     console.log('Global afterAll')
     console.log('Finished with worker')
   }, { scope: 'worker', auto: true }],  // automatically starts for every worker.
